0eeb7e4f670526540557068c74b7ea93733e76e9
April Blair <a.m.blair@eagle.clarion.edu>
2017-01-19 11:53:32 -0500

    test function returns 100 items

--- TEST_arrays.rb
+++ TEST_arrays.rb
@@ -5,8 +5,19 @@ class TestMMFunctions < Minitest::Test
 	def test_one_equals_one()
 		assert_equal(1,1)
 	end
+
+	# tests that our function returns an array.
 	def test_function_returns_array()
+		#sets the variable results as the value of create_mined_minds 
 		results = create_mined_minds_array()
+		# asserts that the class datatype for results is an Array datatype.
 		assert_equal(Array, results.class)
+	end
+		def test_function_returns_100_items()
+			#sets the variable results as the value of create_mined_minds 
+		results = create_mined_minds_array()
+			#
+			assert_equal(100, results.length)
+
 	end
 end
--- arrays.rb
+++ arrays.rb
@@ -1,3 +1,9 @@
 def create_mined_minds_array()
-	[]
+	create_mined_minds_array = []
+	#do something a hundred times.
+	100.times do
+		create_mined_minds_array << 'array item'
+	end
+	#return function
+	create_mined_minds_array
 end
