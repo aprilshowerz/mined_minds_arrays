0a9447a542018de67fa2ff3821f7a6178f2e2d98
April Blair <a.m.blair@eagle.clarion.edu>
2017-01-23 14:43:19 -0500

    added function for 15, 5, &3 and tests for 5, 100, & 17

--- array2.rb
+++ array2.rb
@@ -4,8 +4,18 @@ def hundred_item_array
 	#or try new.array(100)
 
 	hundred_item_array.each_with_index do|value,index|
-		if value %3 == 0
+		if value %15 == 0
+			hundred_item_array[index] = 'mined minds'
+		elsif 
+			value %3 == 0
 			hundred_item_array[index] = 'mined'
+		elsif 
+			value %5 == 0
+			hundred_item_array[index] = 'minds'
+		else
+			#puts
+			value
 		end
+
 	end
 end
--- testarray2.rb
+++ testarray2.rb
@@ -16,4 +16,26 @@ class TestMMFunctions < Minitest::Test
 		results = hundred_item_array
 		assert_equal('mined', results[2])
 	end
+	def test_0_returns_1
+		results = hundred_item_array()
+		assert_equal(1, results[1-1])
+		puts results
+	end
+	def test_2_returns_two
+		results = hundred_item_array()
+		assert_equal(2, results[1])
+		puts results
+	end
+	def test_5_returns_minds
+		results = hundred_item_array
+		assert_equal('minds', results[4])
+	end
+	def test_100_returns_a_minds
+		results = hundred_item_array
+		assert_equal('minds', results.last)
+	end
+	def test_17_returns_17
+		results = hundred_item_array
+		assert_equal(17, results[16])
+	end
 end
